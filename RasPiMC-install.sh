4e4dee7e9d7da9f216ed6cf4d16d4067e9f9b844
timof121 <52547763+timof121@users.noreply.github.com>
2021-11-07 17:36:50 +0100

    yay update

--- RasPiMC-install.sh
+++ RasPiMC-install.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
 
 echo "Welcome! This script installs a minecraft server for you"
-echo "Installing Java 11"
-sudo apt update
-sudo apt install default-jdk -y
+echo "Installing aria2c (a better wget)"
+sudo apt update -y
+sudo apt install aria2c -y
+echo "Installing Java 16"
+sudo apt-get install -y wget apt-transport-https gnupg
+wget https://adoptopenjdk.jfrog.io/adoptopenjdk/api/gpg/key/public
+gpg --no-default-keyring --keyring ./adoptopenjdk-keyring.gpg --import public
+gpg --no-default-keyring --keyring ./adoptopenjdk-keyring.gpg --export --output adoptopenjdk-archive-keyring.gpg
+rm adoptopenjdk-keyring.gpg
+sudo mv adoptopenjdk-archive-keyring.gpg /usr/share/keyrings && sudo chown root:root /usr/share/keyrings/adoptopenjdk-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/adoptopenjdk-archive-keyring.gpg] https://adoptopenjdk.jfrog.io/adoptopenjdk/deb <codename> main" | sudo tee /etc/apt/sources.list.d/adoptopenjdk.list
+sudo apt update -y
+sudo apt install adoptopenjdk-16-jre -y
 echo "Done"
+sleep 1
 echo "Instaling screen"
 sudo apt install screen -y
 echo "Done"
-echo "Getting buildtools"
-wget https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
-echo "Building latest spigot jar, if you want another version, you can do java -jar BuildTools.jar --rev (version)"
+echo "Getting Purpur"
+aria2c https://api.pl3x.net/v2/purpur/1.17.1/latest/download
+sleep 1
+mv purpur* server.jar
 echo "If you want to install another server software, you need to name it server.jar"
 sleep 3
-echo "You need to wait some minutes if you have a Pi 2/3"
-java -jar BuildTools.jar --rev latest
 echo "Done! Now running to generate the server.properties. Then to start the server you can use the start.sh"
-sudo rm -r apache-maven-3.6.0
-sudo chmod u+x start.sh
-sudo rm -r BuildData
-sudo rm -r Spigot
-sudo rm -r CraftBukkit
-sudo rm -r Bukkit
-sudo rm -r work
-sudo cp spigot*.jar server.jar
 echo "Goodbye!"
-echo "If you have any problems/questions, contact me on discord: timof121#1141"
 java -jar server.jar
